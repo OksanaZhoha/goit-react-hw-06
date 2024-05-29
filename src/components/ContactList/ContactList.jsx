d9ceda5bdcc72c955e070ded2847cdac3e6789d6
Oksana Zhoha <145046679+OksanaZhoha@users.noreply.github.com>
2024-05-29 20:51:28 +0300

    Update ContactList.jsx

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,13 +1,20 @@
+import { useSelector } from 'react-redux';
 import Contact from '../Contact/Contact';
 import css from './ContactList.module.css';
 
-export default function ContactList({ contacts, onDelete }) {
+export default function ContactList() {
+  const contacts = useSelector(state => state.contacts.items);
+  const nameContact = useSelector(state => state.filter.name);
+  const filterContacts = contacts.filter(contact =>
+    contact.info.name.toLowerCase().includes(nameContact.toLowerCase())
+  );
+
   return (
     <>
       <ul className={css.contactList}>
-        {contacts.map(contact => (
+        {filterContacts.map(contact => (
           <li className={css.contactListItem} key={contact.id}>
-            <Contact contact={contact} onDelete={onDelete} />
+            <Contact contact={contact} />
           </li>
         ))}
       </ul>
